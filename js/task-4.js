919559f5af6ab48d0d96491cff7ec3f619c89f64
Anastasiia_Hakalo <anastasiiia.hakalo@gmail.com>
2026-03-15 15:19:05 -0600

    fix 3 and 4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,4 @@
-const getTotalBalanceByGender = (users, gender) {
+const getTotalBalanceByGender = (users, gender) => {
   return users
     .filter(user => user.gender === gender)
     .reduce((total, user) => total + user.balance, 0);
